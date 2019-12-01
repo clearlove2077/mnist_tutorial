282333d9a217eec92968bc2473f9734396e8b83b
clearlove2077 <55279665+clearlove2077@users.noreply.github.com>
2019-12-01 22:15:47 +0800

    Add files via upload

---
+++ advanced SVM.py
@@ -0,0 +1,32 @@
+from sklearn.svm import SVC
+import numpy as np
+from sklearn import metrics
+from sklearn.datasets import fetch_mldata
+from sklearn.model_selection import train_test_split
+mnist = fetch_mldata('MNIST original', data_home='./')
+
+# 'mnist.data' is 70k x 784 array, each row represents the pixels from a 28x28=784 image
+# 'mnist.target' is 70k x 1 array, each row represents the target class of the corresponding image
+images = mnist.data
+targets = mnist.target
+
+# make the value of pixels from [0, 255] to [0, 1] for further process
+X = mnist.data / 255.
+Y = mnist.target
+
+# split data to train and test (for faster calculation, just use 1/10 data)
+X_train, X_test, Y_train, Y_test = train_test_split(X[::10], Y[::10], test_size=1000)
+
+test_it=SVC(C=0.006,kernel='linear');
+test_it.fit(X_train,Y_train)
+#train
+
+testresult=test_it.predict(X_test)
+trainresult=test_it.predict(X_train)
+#predict
+
+train_accuracy=metrics.accuracy_score(trainresult,Y_train)
+test_accuracy=metrics.accuracy_score(testresult,Y_test)
+
+print('Training accuracy: %0.2f%%' % (train_accuracy*100))
+print('Testing accuracy: %0.2f%%' % (test_accuracy*100))
